6ddff750990742286023958436cbd68214a52d69
gitty-cyber21 <c14180218@john.petra.ac.id>
2021-02-25 20:21:10 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,45 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
-  jawaban: String = "*****<br>*****<br>*****<br>*****<br>*****";
+  output: String = "*****<br>*****<br>*****<br>*****<br>*****";
+  player : String = "Player 1";
+
+  pemenang : String;
 
   baris = 0;
   kolom = 0;
 
   DONE(){
+      var pisah = this.output.split("<br>");
+
+      var plr = this.player.split(" ");
+      var player = plr[1];
+
+      var arr = new Array();
+
+      for(var i = 0; i < 5; i++){
+        arr[i] = pisah[i].split("");
+      }
+
+      arr[this.baris - 1][this.kolom-1] = player;
+
+      var win = "0";
+
+      var convert = new Array()
+
+      for(var i = 0; i < 5-1; i++){
+        arr[i].push("<br>");
+        convert[i] = arr[i].join("");
+      }
+
+      convert[5-1] = convert[5-1].join("");
+
+      var outputsementara = convert.join("");
+      this.output = outputsementara;
+
       
+      
+
   }
 
 }
