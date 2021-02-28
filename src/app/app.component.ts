8dec199a33c5d6533b7b1b7184b266ad3359747a
gitty-cyber21 <c14180218@john.petra.ac.id>
2021-02-28 22:40:48 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,52 +6,23 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-  output: String = "*****<br>*****<br>*****<br>*****<br>*****";
-  player : String = "Player 1";
-  input : String;
+  output : String[][];
+  baris: 0;
+  kolom: 0;
 
   pemenang : String;
 
-  cetak(){
-    for(var i = 0; i < 5 ; i++){
-      
-    }
+  constructor(){
+      for(var i = 0; i < 5; i++) {
+            this.output[i] = [];
+            for(var j = 0; j< 5; j++) {
+                this.output[i][j] = "*";
+            }
+        }
   }
 
   DONE(){
-      var pisah = this.output.split("<br>");
-
-      var win = "0";
-
-      var convert = new Array()
-
-      var inputsplit = this.input.split(",")
-      var bar = parseInt(inputsplit[0]);
-      var kol = parseInt(inputsplit[1]);
-
-      var plr = this.player.split(" ");
-      var player = plr[1];
-
-      var arr = new Array();
-
-      for(var i = 0; i < 5; i++){
-        arr[i] = pisah[i].split("");
-      }
-
-      arr[bar - 1][kol-1] = player;
-      
-
-
-      for(var i = 0; i < 5-1; i++){
-        arr[i].push("<br>");
-        convert[i] = arr[i].join("");
-      }
-
-      convert[5-1] = convert[5-1].join("");
-
-      var outputsementara = convert.join("");
-      this.output = outputsementara;
+     
 
       
       
